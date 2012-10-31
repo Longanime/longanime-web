8e489386a5ba77964332c91890cebe9ec6947070
Michael Alves Lins <familins@MacBook-Air-de-Michael.local>
2012-10-31 16:32:44 -0200

    Commit inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Longanime Tecnologia - Softwares que evoluem com seu negócio
+
+Desenvolvimento do website da empresa.
+
+[Longanime Tecnologia](http://www.longanime.com.br)
+[*Jesus: Senhor e Salvador*]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LonganimeWeb::Application.config.secret_token = 'e6a36132a2326c35fa91e5fe01cc1545a84b3d073f1c62253e020fe18f6819c46c09ca14dc10c6e01fb8dabeb7d2133143844a601b7ec66f26b603cbb1d29d26'
